106c4e6d453cf759f0cb07022471c116e3d0ab8c
Sejar Mourya <sejarsingr@gmail.com>
2025-09-09 22:48:31 +0530

    add cookie-parser and cors middleware; implement asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: './.env' });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(` 0 Server is running on port : ${process.env.PORT }`);
+  });
+})
+.catch((err) => {
+  console.log("MONGO DB connection faild !!!", err);
+});
+  
 
 /*
 // Uncomment below to use Express and Mongoose directly
